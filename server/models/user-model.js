bff85fee83f323fa3f1d2747d4f19bf69f0e0db8
PRIYARANI-RAI <raipriyarani@gmail.com>
2024-03-09 17:02:57 +0530

    added bcryptjs

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -20,6 +21,21 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false,
+    }
+});
+
+//secure the password with the bcrypt
+userSchema.pre('save', async function(){
+    const user = this;
+    if(user.isModified('password')){
+        next();
+    }
+    try{
+        const saltRound = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash(user.password, saltRound);
+        user.password = hash_password;
+    } catch(e){
+        next(e);
     }
 });
 
