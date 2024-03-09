e3fc9a768d7f4e5b9d2a6464ce9feed9f11b82fe
PRIYARANI-RAI <raipriyarani@gmail.com>
2024-03-09 13:36:04 +0530

    secured connection string in dotenv file

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://localhost:27017/mern_admin";
-
+const URI = process.env.MONGODB_URI;
 const connectDB = async () => {
     try{
         await mongoose.connect(URI);
