21a5efe3ee0e74bd010cfbb3df92adbd59f03107
PRIYARANI-RAI <raipriyarani@gmail.com>
2024-03-09 17:35:19 +0530

    added jwt in app

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,27 +1,34 @@
-const User = require('../models/user-model');
+const User = require("../models/user-model");
 
 // Home Logic
 const home = async (req, res) => {
-    try{
-        res.status(200).json({message: 'Welcome to MERN App'});
-    } catch(e){
-        console.log(e);
-    }
-}
+  try {
+    res.status(200).json({ message: "Welcome to MERN App" });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 //Register
 const register = async (req, res) => {
-    try{
-        const {username, email, phone, password} = req.body;
-        const userExist = await User.findOne({email});
-        if(userExist){
-            return res.status(400).json({msg: "Email already exists"});
-        }
-        const userCreated = await User.create({username, email, phone, password})
-        res.status(200).json({msg: userCreated});
-    } catch(e){
-        res.status(400).json({msg: "Bad Request"});
+  try {
+    const { username, email, phone, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ msg: "Email already exists" });
     }
+    const userCreated = await User.create({ username, email, phone, password });
+    res
+      .status(200)
+      .json({
+        msg: "Registered successfully",
+        token: await userCreated.generateToken(),
+        userId: userCreated._id.toString(),
+      });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ msg: "Bad Request" });
+  }
 };
 
-module.exports = {home, register};
+module.exports = { home, register };
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,45 +1,65 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        require: true,
-    },
-    phone: {
-        type: String,
-        require: true,
-    },
-    password: {
-        type: String,
-        require: true,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    }
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 //secure the password with the bcrypt
-userSchema.pre('save', async function(){
-    const user = this;
-    if(user.isModified('password')){
-        next();
-    }
-    try{
-        const saltRound = await bcrypt.genSalt(10);
-        const hash_password = await bcrypt.hash(user.password, saltRound);
-        user.password = hash_password;
-    } catch(e){
-        next(e);
-    }
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    next();
+  }
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hash_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hash_password;
+  } catch (e) {
+    next(e);
+  }
 });
 
+//json web token
+userSchema.methods.generateToken = async function () {
+  try {
+    return jwt.sign(
+      {
+        userId: this._id.toString(),
+        email: this._email,
+        isAdmin: this._isAdmin,
+      },
+      process.env.JWT_Security_Key,
+      {
+        expiresIn: "30d",
+      }
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 // define the collection name
-const User = new mongoose.model('User', userSchema);
+const User = new mongoose.model("User", userSchema);
 
 module.exports = User;
